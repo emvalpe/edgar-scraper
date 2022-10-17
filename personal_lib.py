a2ac08c97543378d351f8579df3d289f581a2ca6
emvalpe <114688535+emvalpe@users.noreply.github.com>
2022-10-17 15:54:34 -0400

    Update personal_lib.py

--- personal_lib.py
+++ personal_lib.py
@@ -1,3 +1,5 @@
+#Where I will store any useful functions that may or may not be used in every file
+
 import random as r
 
 def random_user_agent(typ="str"):#requests lib is very picky while selenium isn't 
