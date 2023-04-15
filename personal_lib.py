3fc6f58fcd78a04668c93233562a6273ebab1a20
emvalpe <114688535+emvalpe@users.noreply.github.com>
2023-04-15 14:01:19 -0400

    Update personal_lib.py

--- personal_lib.py
+++ personal_lib.py
@@ -1,6 +1,5 @@
-#Where I will store any useful functions that may or may not be used in every file
-
 import random as r
+import re
 
 def random_user_agent(typ="str"):#requests lib is very picky while selenium isn't 
     agents = open("agents.txt", "r")
@@ -10,5 +9,11 @@ def random_user_agent(typ="str"):#requests lib is very picky while selenium isn'
         balls = dict()
         balls["User-Agent"] = str(agent).replace("\n", "")
         return balls
+
+    elif typ == "SEC":
+        balls = dict()
+        balls["User-Agent"] = "Amazon Inc learning@gmail.com"
+        return balls
     else:
         return agent
+    
