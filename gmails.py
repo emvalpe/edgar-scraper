36389fa6906b3c3dd27f3de5ba963bb1482de241
emvalpe <114688535+emvalpe@users.noreply.github.com>
2023-05-19 17:03:04 -0400

    Update gmails.py
    Allowing for more then single word messages, previously just ran "test" as the test case, more complex messages might be nice in the future

--- gmails.py
+++ gmails.py
@@ -3,9 +3,15 @@ import smtplib, ssl, sys
 port = 465  # For SSL
 password = ""
 
+message = ""
+for i in range(len(sys.argv)):
+    if i != 0:
+        message = message + " " + sys.argv[i]
+
+
 # Create a secure SSL context
 context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
     server.login("", password)
-    server.sendmail("", "", sys.argv[1])
+    server.sendmail("", "", message)
