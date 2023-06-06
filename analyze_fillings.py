7f019150549ed8549378c2425cb31eb3141ebc7b
emvalpe <114688535+emvalpe@users.noreply.github.com>
2023-06-06 17:04:14 +0200

    Update analyze_fillings.py
    Made more efficient and added some more colorful output

--- analyze_fillings.py
+++ analyze_fillings.py
@@ -1,15 +1,24 @@
-import personal_lib as personal
 import requests
 import urllib3
-
 from bs4 import BeautifulSoup#needs lxml
+
+import personal_lib as personal
 import time as t
+import random as r
+from colorama import Fore#Fore for text coloration, Back for background etc
 
 import json
-from pathlib import Path
 import os
-
 import spacy
+
+def rainbow(text):
+	new_text = ""
+	color_list = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN, Fore.LIGHTRED_EX, Fore.LIGHTGREEN_EX, Fore.LIGHTYELLOW_EX, Fore.LIGHTBLUE_EX, Fore.LIGHTMAGENTA_EX, Fore.LIGHTCYAN_EX]
+
+	for i in text:
+		new_text += r.choice(color_list)+i
+
+	return new_text
 
 def file_request(url, to=5):
 	file_str = ''
@@ -28,105 +37,129 @@ def file_request(url, to=5):
 
 	return file_str
 
-def find_acqui(sentence):
-	output = 0
-	for tokenized_text in range(len(sentence.ents)):
-		text = sentence.ents[tokenized_text].text
-		if text.find("aqui") != -1:
-			output = tokenized_text
-			break
+def count_of_keywords(body):
+	count = 0
+	locs = []
+	while True:
+		ser = body.find("acqui")
+		if ser == -1:
+			return count, locs
+		else:
+			count += 1
+			locs.append(ser+5)
+			body = body[ser+5:]
 
-	return output
 
 def find_symbols(organization):
-	bad_symbols = ["./", "#", "(", ";"]
+	bad_symbols = ["./", "#", "(", ";", "  ", ", "]
 	loc = 0
 
 	for i in bad_symbols:
-		sec = organization.find(i)
-		if sec != -1 and loc < sec:
-			loc = sec
+		organization.replace(i, "-r-")
 
-	return loc
+	return organization
 
-def connect_orgs(sent, loc):#improve
+def connect_orgs(sent, loc):#setup something to state entities and further check the sentence
 	org = ""
 	sentence = sent.text
 	chunks = []
+	loc = 0
 
 	for i in sent.noun_chunks:
-		chunks.append(i.text)
+		if i.text.find("acqui") != -1:
+			loc = len(chunks)
+		else:
+			chunks.append(i.text)
 	
-	for i in sent.ents:#not catching numbers dates/money also require one proper noun
+	for i in sent.ents:#require one proper noun
 		if sentence.find(i.text) > loc and i.text in chunks and i.label_ == "ORG":
 			org = i.text
+			#print(Fore.LIGHTBLUE_EX+i.text)
 
-	tokens = nlp(org.lower())
+	tokens = nlp(org)#issue here with all caps getting through and things with capital non-proper nouns seems that there isn't a very good solution for this as of now
 	bal = False
 	for val in tokens:
 		if val.tag_ == "NNP":
 			bal = True
+			break
+			#lower_version = nlp(val.text.lower()) #attempt to sort previously mentioned issue
 
 	if bal == False:#some things will get through, particularly country based statements like "Toronto stock exchange"
-		org = ""
+		return ""
+	
+	#print(Fore.WHITE+org)
 
 	return org
 
 def process_filling(response):
 	output = []
 	body = BeautifulSoup(response, "xml").get_text()
-	if body.find("\n") != -1:
+	length = len(body)
+
+	if body.find("\n") != -1:#problematic if a lot of spaces are removed
 		body = body.replace("\n", " ")
 
-	if body.find("acqui") != -1:
-		thingy = len(body)//500000
+	count, locs = count_of_keywords(body)
+	if count != 0:
+		for i in range(0, length, 500000):
+			if len(locs) == 0:
+				break
+			if i*500000 < locs[0] and (i+1)*500000 > locs[0]:
+				rep = []
+				for z in range(len(locs)):
+					if (i+1)*500000 > locs[0]:
+						rep.append(locs[0])#????
+						del locs[0]
 
-		for i in range(thingy+1):
-			if thingy != 0:
 				try:
-					doc = nlp(body[i*500000:(i+1)*500000])
+					nat = body[i*500000:(i+1)*500000]
+					doc = nlp(nat)
+
 				except Exception:
-					doc = nlp(body[i*500000:])
-			else:
-				doc = nlp(body)
+					nat = body[i*500000:]
+					doc = nlp(nat)
 
-			for sentence in doc.sents:
+				for sentence in doc.sents:#could be flawed if a sentence is cut in two
 
-				search = (sentence.text).find("acqui")#expand to merge too
-				if search != -1:
-					company = connect_orgs(sentence, find_acqui(sentence))
+					search = (sentence.text).find("acqui")#expand to "merge" too
+					if search != -1 and (sentence.text).find("reacqui"):
+						#print(Fore.GREEN+sentence.text)
+						company = connect_orgs(sentence, rep[0])#check this function
+						del rep[0]
+						if company.find(", ") != -1 or company.find("  ") != -1:
+							company = company.replace("  ", " ")
+							company = company.replace(", ", " ")
+						#print(Fore.YELLOW+company)
 
-					if company.find(", ") != -1 or company.find("  ") != -1:
-						company = company.replace("  ", " ")
-						company = company.replace(", ", " ")
+						comapny = find_symbols(company)#improved block, test
 
-					locs = find_symbols(company)
+						if company not in output and company != "":
+							x = True
+							for i in output:
+								if company.find(i) != -1 and len(company) > len(i):
+									output[output.index(i)] = company
+									x = False
+									break
 
-					if locs != 0 and company[:locs].find(" ") == -1:
-						company = company[:locs]
-					elif locs != 0 and company[:locs].find(" ") != -1:
-						company = company[locs:]
-
-					if company not in output and company != "":
-						x = True
-						for i in output:
-							if company.find(i) != -1:#dont add if company in element
-								output[output.index(i)] = company
-								x = False
-								break
-
-						if x == True:
-							output.append(company)
+							if x == True:
+								output.append(company)
 	
 	return output
 
-def analyze(file, output):#check filing type
+def analyze(file, output, total):
+	ite = 0
+	start = t.time()
 	while True:
 		line = file.readline()
 		if line == "" or line == "\n":continue
 		if not line:return
+		ite+=1
 		company = json.loads(line)
-		
+		#print(rainbow(company["name"]))
+		if ite%100 == 0:
+			end = t.time()
+			print(Fore.GREEN+str(ite)+"\\"+str(total)+" with a time(min): "+str((end-start)/60) + " rate of(comapnies/sec): "+str(ite/(end-start)))
+
 		iterat = 0
 		for i in company["filings"]["recent"]["accessionNumber"]:
 			if company["filings"]["recent"]["form"][iterat] in forms:
@@ -141,39 +174,48 @@ def analyze(file, output):#check filing type
 					if owned != []:
 						for o in owned:
 							if o not in company["acquired"]:
-								company["acquired"].append(i + ":" + o)#write name of aquired company and filing
+								company["acquired"].append(i + ":" + o)
 						
-						output.write("\n"+json.dumps(company))#output per company, minimize footprint on memory
-						print("Hits for %s:%s" % (company["name"], str(company["acquired"])))	
+						output.write("\n"+json.dumps(company))
+							
 			iterat += 1
+
+		if company["acquired"] != []: print(Fore.YELLOW + "Hits for %s:%s" % (company["name"], str(company["acquired"])))	
 
 	output.close()
 		
 	
 headers = personal.random_user_agent("SEC")
-forms = ["10-K", "6-K"]#removed 8k and 10q, seems to still catch everything based on current testing
+forms = ["10-K", "6-K", "8k"]#removed 10q, seems to still catch everything based on current testing
 
 nlp = spacy.load("en_core_web_sm")
-p = Path("./").iterdir()
 iterat = 0
 start = t.time()
 
-print("Starting")
-for file in p:
-	new_info = []
-	if (file.name).find("processed") != -1:
-		with file.open() as fr:
-			try:#remove previous output file
-				os.remove(str(file.name).replace("processed", "final"))
-			except FileNotFoundError:
-				pass
+temp = open("processed.json", "r")
+total = 0
+while True:
+	line = temp.readline()
+	if not line:break
+	else:total+=1
+del temp
 
-			analyze(fr, open((file.name).replace("processed", "final"), "a+"))
-			fr.close()
+print(rainbow("Total Entities: ")+str(total))
+
+file = open("processed.json", "r")
+
+try:
+	os.remove(str(file.name).replace("processed", "final"))
+except FileNotFoundError:
+	pass
+
+analyze(file, open("final.json", "a+"), total)
+file.close()
+			
 os.system("python3 emails.py completed the SEC processing")
 
 '''
 Issues:
- - mentioned some issues in comments
- - utf-8 encoding doesn't work for things like spanish chars, going to have to expand that
+ - connect_org function has some issues with the ML network being used stated in code also remove SEC things
+ - utf-8 encoding doesn't work for things like spanish chars how do I even change this
 '''
