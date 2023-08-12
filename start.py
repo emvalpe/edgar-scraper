9eec26b34eb0efd2f263dc05ce9e3e3a0d19410b
emvalpe <114688535+emvalpe@users.noreply.github.com>
2023-08-12 17:43:40 -0500

    Create start.py

---
+++ start.py
@@ -0,0 +1,16 @@
+import requests
+import urllib3
+from bs4 import BeautifulSoup#needs lxml
+
+import personal_lib as personal
+import time as t
+import random as r
+from colorama import Fore#Fore for text coloration, Back for background etc
+
+import json
+import os
+import spacy
+
+import analyze_fillings
+
+analyze_fillings.start()
